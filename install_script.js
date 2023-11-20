5e6b5125446d5d9488a2a404e0f5bb2ca27b783d
ancelee <ancelee@163.com>
2023-11-20 14:31:25 +0800

    edit install

--- install_script.js
+++ install_script.js
@@ -2,7 +2,7 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: brown; icon-glyph: download;
 
- const scripts = [
+const scripts = [
   {
     moduleName: "MyTest",
     url:
@@ -10,12 +10,27 @@
   },
 ];
 // Install Scripts.js
-const $ = importModule("Env");
+// const $ = importModule("Env");
 function update() {
   log("🔔更新脚本开始!");
   scripts.forEach((script) => {
-    $.getFile(script);
+    getFile(script);
   });
   log("🔔更新脚本结束!");
 }
+/**
+ * @description 下载文件
+ * @param {*} param0 
+ */
+ const getFile = async ({moduleName, url}) => {
+  log(`开始下载文件: 🌝 ${moduleName}`)
+  const header = `// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-gray; icon-glyph: file-code;\n`;
+  const content = await getStr({url})
+  const fileHeader = content.includes('icon-color') ? `` : header
+  write(`${moduleName}`, `${fileHeader}${content}`)
+  log(`文件下载完成: 🌚 ${moduleName}`)
+}
+
 update();
